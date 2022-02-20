5d406293098131a7518151baa7d81bd6c50dfb50
Islombek Turakulov <iuturakulov@edu.hse.ru>
2022-02-21 02:51:26 +0300

    Moved selection sort to the new .cpp file

---
+++ SelectionSort.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Islam on 21.02.2022.
+//
+
+#include <algorithm>
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min_index = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_index]) {
+                min_index = j;
+            }
+        }
+        std::swap(arr[min_index], arr[i]);
+    }
+}
